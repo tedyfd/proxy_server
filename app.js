5d0d0733b0c05ca55ce777b90f08ffa9c3c49707
Tedy Fernando <teddyfernando45@gmail.com>
2023-10-12 14:50:08 +0700

    update

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+const port = 3000;
 
-app.use('/api', createProxyMiddleware({ target: 'http://192.168.101.1:8081', changeOrigin: true }));
+// Define your proxy target
+const proxyTarget = 'http://192.168.101.1:8081';
 
-app.listen(3001, () => {
-  console.log('Proxy server running on port 3001');
+// Create a proxy middleware with CORS
+const proxyOptions = {
+  target: proxyTarget,
+  changeOrigin: true,
+  onProxyRes: (proxyRes, req, res) => {
+    // Set CORS headers for the proxy response
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  },
+};
+
+const apiProxy = createProxyMiddleware(proxyOptions);
+
+// Use the proxy middleware for a specific path
+app.use('/api', apiProxy);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
